941ac90c82833cda51cd06c17e6fda10aa90f568
William Z <williazz@usc.edu>
2021-12-12 15:01:49 -0800

    Removed accidental copies of files

---
+++ test/models/countdown_test 2.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mvp/models/countdown.dart';
+import 'package:mvp/models/task.dart';
+import 'package:mvp/util/seed.dart';
+
+void main() {
+  group('CountdownModel', () {
+    final CountdownModel countdown = CountdownModel();
+    final TaskModel task = SeedTasks.DeadBugs();
+
+    setUp(() {
+      countdown.selectTask(task);
+    });
+
+    tearDown(() {
+      countdown.clearTask();
+    });
+
+    test('.start() increases elapsed', () async {
+      countdown.start();
+      await Future.delayed(const Duration(seconds: 1), () {
+        expect(countdown.elapsed.inMilliseconds, greaterThan(0));
+      });
+    });
+
+    test('.complete() finishes the countdown', () {
+      expect(countdown.isDone, false);
+      countdown.complete();
+      expect(countdown.isDone, true);
+      expect(countdown.remaining, Duration.zero);
+    });
+  });
+}
