9f52e0f5c98749524f7e259caa2b4f0cfd7a98b5
William Z <williazz@usc.edu>
2021-12-15 19:47:12 -0800

    Added feature scroll-to-index  - However, the last items in the list do not behave as intended. Must find solution

--- lib/components/do_routine/list.dart
+++ lib/components/do_routine/list.dart
@@ -1,27 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 import 'package:mvp/components/do_routine/task.dart';
 import 'package:mvp/models/do_routine.dart';
 import 'package:mvp/models/routine.dart';
-import 'package:provider/provider.dart';
 
-class DoRoutineListWidget extends StatelessWidget {
+class DoRoutineListWidget extends StatefulWidget {
   const DoRoutineListWidget({Key? key}) : super(key: key);
+
+  @override
+  _DoRoutineListWidgetState createState() => _DoRoutineListWidgetState();
+}
+
+class _DoRoutineListWidgetState extends State<DoRoutineListWidget> {
+  final ItemScrollController itemScrollController = ItemScrollController();
+  late final int initialScrollIndex;
+  late int prevIndex;
+
+  void scrollToIndex(int index) {
+    itemScrollController.scrollTo(
+        index: index,
+        duration: const Duration(milliseconds: 700),
+        curve: Curves.easeInOutCubic);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    late final int index =
+        Provider.of<DoRoutineModel>(context, listen: false).index;
+    initialScrollIndex = index;
+    prevIndex = index;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final int index = Provider.of<DoRoutineModel>(context).index;
+    if (prevIndex != index) {
+      // print('didChangeD: $index');
+      scrollToIndex(index);
+      prevIndex = index;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Selector<DoRoutineModel, RoutineModel>(
-      selector: (_, DoRoutineModel doRoutine) => doRoutine.routine!,
-      builder: (_, RoutineModel routine, __) => Selector<DoRoutineModel, int>(
-          selector: (_, DoRoutineModel doRoutine) => doRoutine.index,
-          builder: (BuildContext context, int currentIndex, __) {
-            return ListView.builder(
-              itemCount: routine.length,
-              itemBuilder: (BuildContext context, int taskIndex) {
-                return TaskWidget(
-                    taskIndex: taskIndex, task: routine.atIndex(taskIndex)!);
-              },
-            );
-          }),
-    );
+        selector: (_, DoRoutineModel doRoutine) => doRoutine.routine!,
+        builder: (_, RoutineModel routine, __) => Selector<DoRoutineModel, int>(
+            selector: (_, DoRoutineModel doRoutine) => doRoutine.index,
+            builder: (BuildContext context, int currentIndex, __) {
+              return Expanded(
+                  child: ScrollablePositionedList.builder(
+                      itemScrollController: itemScrollController,
+                      itemCount: routine.length,
+                      initialScrollIndex: initialScrollIndex,
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, int taskIndex) {
+                        return TaskWidget(
+                            taskIndex: taskIndex,
+                            task: routine.atIndex(taskIndex)!);
+                      }));
+            }));
   }
 }
