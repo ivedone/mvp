5e9183918b23c4af3cdf48df313e2da17f95004d
William Z <williazz@usc.edu>
2022-01-25 14:41:09 -0800

    Implemented dependency injection by hand

--- lib/app.dart
+++ lib/app.dart
@@ -1,25 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 import 'package:mvp/screens/home.dart';
 import 'package:mvp/theme/theme.dart';
-
-import 'models/countdown.dart';
-import 'models/do_routine.dart';
-import 'models/audio_notifier/audio_notifier.dart';
 
 class IvedoneApp extends StatelessWidget {
   const IvedoneApp({Key? key}) : super(key: key);
 
-  _forceLazyLoadingOfDependencies(BuildContext context) {
-    Provider.of<CountdownModel>(context, listen: false);
-    Provider.of<DoRoutineModel>(context, listen: false);
-    Provider.of<AudioNotifier>(context, listen: false);
-  }
-
   @override
   Widget build(BuildContext context) {
-    _forceLazyLoadingOfDependencies(context);
     return MaterialApp(
         title: 'ivedone',
         theme: activeTheme,
