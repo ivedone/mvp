836e5d00cb94db37cdd35759382b8d904bc04fb8
William Z <williazz@usc.edu>
2022-01-28 19:40:49 -0800

    Double tap to skip10Sec with animations

--- lib/components/do_routine/detail/detail.dart
+++ lib/components/do_routine/detail/detail.dart
@@ -16,10 +16,10 @@ class DoRoutineDetailWidget extends StatefulWidget {
 }
 
 class _DoRoutineDetailWidgetState extends State<DoRoutineDetailWidget> {
-  Timer? _timer;
+  Timer? _showControlsTimer;
 
-  late bool _show;
-  bool get show => _show;
+  late bool _showControls;
+  bool get showControls => _showControls;
 
   late final DoRoutineModel doRoutineMethods;
   late bool _isPaused;
@@ -37,7 +37,7 @@ class _DoRoutineDetailWidgetState extends State<DoRoutineDetailWidget> {
     super.initState();
     doRoutineMethods = Provider.of<DoRoutineModel>(context, listen: false);
     _isPaused = doRoutineMethods.isPaused;
-    _show = _isPaused;
+    _showControls = _isPaused;
   }
 
   @override
@@ -50,7 +50,7 @@ class _DoRoutineDetailWidgetState extends State<DoRoutineDetailWidget> {
       _isPaused = willPause;
       if (didPause) {
         cancelTimer();
-        _show = true;
+        _showControls = true;
       } else if (didPlay) {
         hideInThreeSeconds();
       }
@@ -58,28 +58,34 @@ class _DoRoutineDetailWidgetState extends State<DoRoutineDetailWidget> {
   }
 
   cancelTimer() {
-    _timer?.cancel();
+    _showControlsTimer?.cancel();
   }
 
   hideInThreeSeconds() {
     cancelTimer();
-    _timer = Timer(const Duration(seconds: 3), () {
+    _showControlsTimer = Timer(const Duration(seconds: 3), () {
       setState(() {
-        _show = false;
+        _showControls = false;
       });
+    });
+  }
+
+  hide() {
+    setState(() {
+      _showControls = false;
     });
   }
 
   toggleShow() {
     setState(() {
-      if (show) {
+      if (showControls) {
         cancelTimer();
       } else {
         if (!isPaused) {
           hideInThreeSeconds();
         }
       }
-      _show = !show;
+      _showControls = !showControls;
     });
   }
 
@@ -93,12 +99,26 @@ class _DoRoutineDetailWidgetState extends State<DoRoutineDetailWidget> {
           Expanded(
               child: Stack(children: [
             const DetailContentWidget(),
+            Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+              Expanded(
+                  child: Center(
+                      child: AnimatedOpacity(
+                          opacity: showSkip10SecBack ? 1 : 0,
+                          duration: const Duration(milliseconds: 200),
+                          child: _skip10Sec(false)))),
+              Expanded(
+                  child: Center(
+                      child: AnimatedOpacity(
+                          opacity: showSkip10SecForwards ? 1 : 0,
+                          duration: const Duration(milliseconds: 200),
+                          child: _skip10Sec(true)))),
+            ]),
             _gestureDetector(ignoreMirrorsShow: true),
             _gestureDetector(
                 ignoreMirrorsShow: false,
                 child: AnimatedOpacity(
-                  opacity: show ? 1 : 0,
-                  duration: const Duration(milliseconds: 200),
+                  opacity: showControls ? 1 : 0,
+                  duration: const Duration(milliseconds: 300),
                   child: const DetailControlsWidget(),
                 )),
           ])),
@@ -115,7 +135,7 @@ class _DoRoutineDetailWidgetState extends State<DoRoutineDetailWidget> {
     Widget? child,
   }) {
     return IgnorePointer(
-        ignoring: ignoreMirrorsShow ? show : !show,
+        ignoring: ignoreMirrorsShow ? showControls : !showControls,
         child: GestureDetector(
             onTap: toggleShow,
             onDoubleTap: () {},
@@ -124,13 +144,76 @@ class _DoRoutineDetailWidgetState extends State<DoRoutineDetailWidget> {
   }
 
   _handleDoubleTapDown(TapDownDetails details) {
+    hide();
     final double width = context.size!.width;
     final double dx = details.localPosition.dx;
-    final isLeft = (dx / width) < 0.5;
-    if (isLeft) {
-      doRoutineMethods.skipBack10Sec();
-    } else {
+    final forwards = (dx / width) > 0.5;
+    _showSkip10SecAndHide(forwards);
+    if (forwards) {
       doRoutineMethods.skipForward10Sec();
+    } else {
+      doRoutineMethods.skipBack10Sec();
+    }
+  }
+
+  _skip10Sec(bool forwards) {
+    return Center(
+        child: SizedBox(
+            width: 150,
+            height: 150,
+            child: Container(
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromRGBO(0, 0, 0, 0.5)),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Icon(forwards ? Icons.fast_forward : Icons.fast_rewind),
+                      const Text('10 sec'),
+                    ]))));
+  }
+
+  Timer? _skip10SecForwardsTimer;
+  bool _showSkip10SecForwards = false;
+  bool get showSkip10SecForwards => _showSkip10SecForwards;
+
+  Timer? _skip10SecBackTimer;
+  bool _showSkip10SecBack = false;
+  bool get showSkip10SecBack => _showSkip10SecBack;
+
+  _clearSkip10SecTimer(bool forwards) {
+    if (forwards) {
+      _skip10SecForwardsTimer?.cancel();
+    } else {
+      _skip10SecBackTimer?.cancel();
+    }
+  }
+
+  _showSkip10SecAndHide(bool forwards) {
+    _clearSkip10SecTimer(forwards);
+    setState(() {
+      if (forwards) {
+        _showSkip10SecForwards = true;
+      } else {
+        _showSkip10SecBack = true;
+      }
+    });
+
+    final hide = Timer(const Duration(milliseconds: 500), () {
+      setState(() {
+        if (forwards) {
+          _showSkip10SecForwards = false;
+        } else {
+          _showSkip10SecBack = false;
+        }
+      });
+    });
+
+    if (forwards) {
+      _skip10SecForwardsTimer = hide;
+    } else {
+      _skip10SecBackTimer = hide;
     }
   }
 }
